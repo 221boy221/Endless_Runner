2c44b998983cfd235d61a76ee2488162288685b0
Ramses di Perna <15826@ma-web.nl>
2014-05-12 19:16:31 +0200

    Added moving background and deleted test objects. Also made a view new maps

---
+++ Assets/Scripts/Background/movingBack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingBack : MonoBehaviour {
+	public float speed = 0;
+	public static movingBack current;
+
+	float pos = 0;
+
+	// Use this for initialization
+	void Start () {
+		current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos += speed * Time.deltaTime;
+		if(pos > 1.0f)
+			pos = 0f;
+
+		renderer.material.mainTextureOffset = new Vector2(pos,0);
+	}
+}
