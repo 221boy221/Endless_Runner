64d625e9a65b5ac327331084887a835e3e7661ca
Ramses di Perna <15826@ma-web.nl>
2014-05-12 19:48:04 +0200

    EnemySpawner Version 0.01.

---
+++ Assets/Scripts/GameController/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemy1;
+	public GameObject enemy2;
+
+	void Start () {
+
+		Invoke ("EnemySpawn",5f);
+	}
+
+	void SpawnEnemy(GameObject EnemyType){
+
+		Instantiate (EnemyType, new Vector2 (1, 0), this.transform.rotation);
+	}
+
+	void EnemySpawn(){
+
+		int chooseEnemyToSpawn = Random.Range (1, 3);
+		Debug.Log (chooseEnemyToSpawn);
+		if (chooseEnemyToSpawn == 1) {
+			SpawnEnemy(enemy1);
+		} else if (chooseEnemyToSpawn == 2) {
+			SpawnEnemy(enemy2);
+		}
+		Invoke ("EnemySpawn",Random.Range(3f,6f));
+	}
+}
