ad4b930215ed20f080474e7c9d4eb820e324f024
Ramses di Perna <15826@ma-web.nl>
2014-05-14 09:55:40 +0200

    mappen voor kogels toegevoegd en script voor getDamage en weakness geschreven

--- Assets/Scripts/Enemies/Enemy1Behaviour.cs
+++ Assets/Scripts/Enemies/Enemy1Behaviour.cs
@@ -5,15 +5,33 @@ public class Enemy1Behaviour : MonoBehaviour {
 
 	public GlobalEnemyScript scriptEnemy;
 
+	public string weakness; // <-- tag van kogel waar hij niet tegen kan.
+
 	void Update(){
 		Movement ();
 
 		if (this.transform.position.x < -6.7) {
 			Destroy(this.gameObject);
 		}
+		if (scriptEnemy.health <= 0) {
+			Death();
+		}
 	}
 
-	void Movement(){
+	virtual protected void Movement(){
 		transform.Translate (new Vector2(-scriptEnemy.movementSpeed,0)*Time.deltaTime,Space.World);
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == weakness) {
+			Debug.Log("auw!!");
+		}
+	}
+
+	void GetDamage(float dmg){
+
+	}
+
+	void Death(){
+		Destroy (this.gameObject);
 	}
 }
--- Assets/Scripts/Enemies/GlobalEnemyScript.cs
+++ Assets/Scripts/Enemies/GlobalEnemyScript.cs
@@ -4,5 +4,6 @@ public class GlobalEnemyScript{
 	public float movementSpeed;
 	public float health;
 	public float attackDamage;
+	public float scoreValue;
 
 }
