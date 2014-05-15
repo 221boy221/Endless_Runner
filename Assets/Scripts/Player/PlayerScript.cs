3be9ef89fe3f459b98b941bed2bb4bc1a64d7fa4
Boy Voesten <warrock.boy@hotmail.com>
2014-05-15 21:34:42 +0200

    Added: player rotation to mouse, bullet spawn on click, bullet pos facing mouse pos.

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+    private GameObject objPlayer;
+    public GameObject objGunNozzle;
+
     //input variables (variables used to process and handle input)
     private Vector3 inputRotation;
     private Vector3 inputMovement;
@@ -12,11 +15,15 @@ public class PlayerScript : MonoBehaviour {
     private Vector3 tempVector2;
 
     // Player movement
-    private float moveSpeed = 100f;
+    private float moveSpeed = 100.0f;
+
+    private VariableScript ptrScriptVariable;
+    public Camera c;
 
     // Use this for initialization
     void Start() {
-
+        objPlayer = (GameObject)GameObject.FindWithTag("Player");
+		ptrScriptVariable = (VariableScript) objPlayer.GetComponent( typeof(VariableScript) );
     }
 
     // Update is called once per frame
@@ -26,25 +33,42 @@ public class PlayerScript : MonoBehaviour {
     }
 
     void FindPlayerInput() {
-        // find vector to the mouse
-        inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        // the position of the middle of the screen
-        tempVector2 = new Vector3(Screen.width * 0.5f, 0, Screen.height * 0.5f);
-        /* find the position of the mouse on screen */
+        // Find vector to Mouse - (Will be used later on for the bullets)
+            //inputMovement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0); // X - Y - Z
+        // The position of the middle of the screen
+        tempVector2 = new Vector3(this.transform.position.x, this.transform.position.y, 0 ); // X - Y - Z
+        // Go from units to pixels
+        tempVector2 = c.WorldToScreenPoint(tempVector2);
+        tempVector2.z = 0;
+        // Find the position of the mouse on screen
         tempVector = Input.mousePosition;
-        /* input mouse position gives us 2D coordinates, I am moving the Y coordinate to the Z coorindate in temp Vector and setting the Y 
-        coordinate to 0, so that the Vector will read the input along the X (left and right of screen) and Z (up and down screen) axis, and not the X and Y (in and out of screen) axis*/
-        tempVector.z = tempVector.y;
-        tempVector.y = 0;
-        Debug.Log(tempVector);
-        // the direction we want face/aim/shoot is from the middle of the screen to where the mouse is pointing
-        inputRotation = tempVector - tempVector2; 
+        // Input.mousePosition gives 2D coordinates ( X - Y )
+        Debug.Log("tempVec2 = " + tempVector2);
+        Debug.Log("tempVec = " + tempVector);
+        // The direction it aims at (is currently from the middle of the screen to where the mouse is pointing)
+        inputRotation = tempVector - tempVector2;
+        //Debug.Log("inputRot = " + inputRotation);
+
+        // Spawn and fire the bullets
+        if (Input.GetMouseButtonDown(0)) {
+            HandleBullets();
+        }
+    }
+
+    void HandleBullets() {
+        // Get the inputRotation Vector from the player's position to the mouse by multiplying the inputRotation Vector by the Quaternion.AngleAxis.
+        tempVector = Quaternion.AngleAxis(8f, Vector3.up) * inputRotation;
+        // Adds offset - (For the Gun Nozzle)
+        tempVector = (objGunNozzle.transform.position + (tempVector.normalized * 0.8f));
+        // Create a bullet and rotate it based on the vector inputRotation
+        Instantiate(ptrScriptVariable.objBullet, tempVector, Quaternion.LookRotation(inputRotation));
     }
 
     void ProcessMovement() {
-        rigidbody.AddForce(inputMovement.normalized * moveSpeed * Time.deltaTime);
+
         transform.rotation = Quaternion.LookRotation(inputRotation);
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
-        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-    }
+        //transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+        transform.position = new Vector3(transform.position.x, 0, 0);
+    }
+
 }
