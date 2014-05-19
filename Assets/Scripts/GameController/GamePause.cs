da2dd0aed17b54e73c2dcfd0d19bde88e29bd41e
Boy Voesten <warrock.boy@hotmail.com>
2014-05-19 02:07:49 +0200

    Fixed still being able to aim and shoot while the game was paused.

--- Assets/Scripts/GameController/GamePause.cs
+++ Assets/Scripts/GameController/GamePause.cs
@@ -7,7 +7,8 @@ public class GamePause:MonoBehaviour {
 	[SerializeField] private GUIStyle labelStyle;
 
 	private bool paused = false;
-
+    
+    
     void Awake() {
         if (Time.timeScale == 0) {
             Time.timeScale = 1;
@@ -24,7 +25,7 @@ public class GamePause:MonoBehaviour {
         if (!paused) {
             return;
         }
-			
+        
 		GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3 ((float)Screen.width / 1920.0f, (float)Screen.height / 1080.0f, 1));
 
 		GUI.Box(new Rect(-1, -1, 1922, 1082), new GUIContent());
@@ -38,7 +39,11 @@ public class GamePause:MonoBehaviour {
     }
 
     private void TogglePause() {
-		Time.timeScale = (Time.timeScale == 0) ? 1 : 0;
+        Object[] objects = FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject go in objects) {
+            go.SendMessage("GamePause", SendMessageOptions.DontRequireReceiver);
+        }
+        Time.timeScale = (Time.timeScale == 0) ? 1 : 0; // If it's 0 make it 1, else if 1 make it 0
 		paused = Time.timeScale == 0;
     }
 
--- Assets/Scripts/Weapons/WeaponRotation.cs
+++ Assets/Scripts/Weapons/WeaponRotation.cs
@@ -5,38 +5,38 @@ public class WeaponRotation : MonoBehaviour {
 
 	//hieronder staan alle kogels van alle wapens.
 	public GameObject bullet1; 
-
-	//---------------------------\\
 	private int currentWeapon = 1; //hiermee houd ik bij welk wapen de speler heeft
+    protected bool paused;
+    Vector2 mousePos;
 
+    void GamePause() {  // This function will run when the player presses esc, because of the GamePause.cs
+        paused = (paused == false) ? true : false;  // This is basically: if (paused = true) paused = false; else paused = true;
+    }
 
-	Vector2 mousePos;
-
-	// Update is called once per frame
 	void Update () {
-		// Je hoeft niet de camera een public variable te maken. Hiermee kan je hem aanspreken en voorkom je errors als je de speler moet weghalen.
-		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position); // <-- hier bereken ik de positie van de speler in de camera view
+        if (!paused) { // If the game is not paused, continue.
+            // Je hoeft niet de camera een public variable te maken. Hiermee kan je hem aanspreken en voorkom je errors als je de speler moet weghalen.
+		    Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position); // <-- hier bereken ik de positie van de speler in de camera view
 		
-		mousePos.x = Input.mousePosition.x - objectPos.x; // in de x van de vector 2 van de variable mousePos stop ik de x van de muis - de x van de speler waar hij zich op het scherm bevind.
-		mousePos.y = Input.mousePosition.y - objectPos.y; // in de y van de vector 2 van de variable mousePos stop ik de y van de muis - de y van de speler waar hij zich op het scherm bevind.
+		    mousePos.x = Input.mousePosition.x - objectPos.x; // in de x van de vector 2 van de variable mousePos stop ik de x van de muis - de x van de speler waar hij zich op het scherm bevind.
+		    mousePos.y = Input.mousePosition.y - objectPos.y; // in de y van de vector 2 van de variable mousePos stop ik de y van de muis - de y van de speler waar hij zich op het scherm bevind.
 
-		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg; // de hoek(angle) bereken ik met wat we bij wiskunde hadden geleerd die dag. de atan2 functie en dan Rad2Deg(* 180 / PI)
+		    float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg; // de hoek(angle) bereken ik met wat we bij wiskunde hadden geleerd die dag. de atan2 functie en dan Rad2Deg(* 180 / PI)
 
-		//Debug.Log (angle); 
+		    //(ik weet niet precies wat de "Quaternion.Euler" doet maar ik had gelezen dat dat de z stuf extra fixte en bugs voorkwam)
+		    transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle)); // hier stop ik de rotatie in actie door de waarde van de rotation te veranderen 
 
-		//(ik weet niet precies wat de "Quaternion.Euler" doet maar ik had gelezen dat dat de z stuf extra fixte en bugs voorkwam)
-		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle)); // hier stop ik de rotatie in actie door de waarde van de rotation te veranderen 
-
-		//Hieronder is een placeholder voor het schieten. Er moet een cooldown etc etc komen maar het was alleen voor het uittesten.
-		if (Input.GetKeyDown (KeyCode.Mouse0)){
-			SchootBullet(currentWeapon);
-		}
+		    //Hieronder is een placeholder voor het schieten. Er moet een cooldown etc etc komen maar het was alleen voor het uittesten.
+		    if (Input.GetKeyDown (KeyCode.Mouse0)){
+			    SchootBullet(currentWeapon);
+		    }
+        }
 	}
 	
-	void SchootBullet(int weapon){
-		if (weapon == 1) {
-			Instantiate (bullet1, this.transform.position, this.transform.rotation); // hier word de kogel gemaakt. kijk in het kogel script hoe ik hem laat bewegen.("bulletsScript") ^^  
-		}
+	void SchootBullet(int weapon) {
+        if (weapon == 1) {
+            Instantiate (bullet1, this.transform.position, this.transform.rotation); // hier word de kogel gemaakt. kijk in het kogel script hoe ik hem laat bewegen.("bulletsScript") ^^  
+        } 
 	}
 	
 }
