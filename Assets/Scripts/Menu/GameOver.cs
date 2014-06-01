7ab03417a9eea61088db808f5b1266eb89426bd1
Boy Voesten <warrock.boy@hotmail.com>
2014-06-01 15:43:06 +0200

    Added gameover scene with script

---
+++ Assets/Scripts/Menu/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    public Texture gameOverTexture;
+
+    void OnGUI() {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), gameOverTexture);
+        if (GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2, 150, 25), "Try again")) {
+            Application.LoadLevel("testScene");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 -75, Screen.height / 2 + 30, 150, 25), "Main Menu")) {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
