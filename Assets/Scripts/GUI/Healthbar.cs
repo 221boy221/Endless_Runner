4fcc30a21e15445ae81ec4ef2a84a087d57a7479
Boy Voesten <warrock.boy@hotmail.com>
2014-06-01 20:46:52 +0200

    Repositioned the weapons, removed playerHealthUI text, fixed AI healthbar bug.

--- Assets/Scripts/GUI/Healthbar.cs
+++ Assets/Scripts/GUI/Healthbar.cs
@@ -15,7 +15,9 @@ public class Healthbar : MonoBehaviour {
 	void Start() {
         playerHealth = GameObject.FindGameObjectWithTag("PlayerHealthUI").GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyBehaviour>();
-        if (enemyHealth != null) {
+        if (gameObject.tag == "Player") {
+            maxHealth = playerHealth.Health;
+        } else if (enemyHealth != null) {
             maxHealth = enemyHealth.Health;
         }
 	}
@@ -27,7 +29,7 @@ public class Healthbar : MonoBehaviour {
 	void OnGUI() {
 		GUI.DrawTexture(new Rect(position.x - 45, (Screen.height + -position.y) - 75, hbBackground.width / 7, hbBackground.height / 5), hbBackground);
         if (gameObject.tag == "Player") {
-            GUI.DrawTexture(new Rect((position.x + 16) - 45, ((Screen.height + -position.y) + 3) - 75, (hpBar.width / 7) / (100 / playerHealth.Health), hpBar.height / 5), hpBar);
+            GUI.DrawTexture(new Rect((position.x + 16) - 45, ((Screen.height + -position.y) + 3) - 75, (hpBar.width / 7) / (maxHealth / playerHealth.Health), hpBar.height / 5), hpBar);
         } else {
             GUI.DrawTexture(new Rect((position.x + 16) - 45, ((Screen.height + -position.y) + 3) - 75, (hpBar.width / 7) / (maxHealth / enemyHealth.Health), hpBar.height / 5), hpBar);
         }
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,16 +5,8 @@ public class PlayerHealth : MonoBehaviour {
 
     private float health = 100.0f;
 
-    private GUIText healthText;
-
-    void Awake() {
-        healthText = GameObject.FindGameObjectWithTag("PlayerHealthUI").GetComponent<GUIText>();
-        healthText.text = "Health: " + health;
-    }
-
     public void TakeDamage(float dmg) {
         health -= dmg;
-        healthText.text = "Health: " + health;
 
         if (health <= 0) {
             Dead();
