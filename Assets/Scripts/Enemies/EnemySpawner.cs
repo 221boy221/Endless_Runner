339ad3635b818f09b7544b41f1ce8e52a83098cf
Ramses di Perna <15826@ma-web.nl>
2014-06-02 15:42:19 +0200

    Made the spawn system better for better gameplay and made enemies trigger colliders so they pass through each other

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -5,6 +5,8 @@ public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemy1;
 	public GameObject enemy2;
+	public GameObject enemy1Num2;
+	public GameObject enemy2Num2;
 
 	void Start() {
 
@@ -16,17 +18,32 @@ public class EnemySpawner : MonoBehaviour {
 		Instantiate(EnemyType, new Vector2 (7, 0), this.transform.rotation);
 	}
 
+	void Spawn2Enemies(GameObject EnemyType1,GameObject EnemyType2){
+
+		Instantiate(EnemyType1, new Vector2 (7, 0), this.transform.rotation);
+		Instantiate(EnemyType2, new Vector2 (10, 0), this.transform.rotation);
+	}
+
 	void EnemySpawn() {
 
+		int chooseSpawnType = Random.Range (1, 3);
 		int chooseEnemyToSpawn = Random.Range(1, 3);
 
 		Debug.Log(chooseEnemyToSpawn);
-
-		if (chooseEnemyToSpawn == 1) {
-			SpawnEnemy(enemy1);
-		} else if (chooseEnemyToSpawn == 2) {
-			SpawnEnemy(enemy2);
+		if (chooseSpawnType == 1) {
+			if (chooseEnemyToSpawn == 1) {
+				SpawnEnemy (enemy1);
+			} else if (chooseEnemyToSpawn == 2) {
+				SpawnEnemy (enemy2);
+			}						
+		} else if (chooseSpawnType == 2) {
+			if (chooseEnemyToSpawn == 1) {
+				Spawn2Enemies (enemy1,enemy2);
+			} else if (chooseEnemyToSpawn == 2) {
+				Spawn2Enemies (enemy2,enemy1);
+			}	
 		}
-		Invoke("EnemySpawn",Random.Range(3f,6f));
+
+		Invoke("EnemySpawn",Random.Range(5f,7f));
 	}
 }
