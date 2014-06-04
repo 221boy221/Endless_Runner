76638527d2cdec545bd161dcfe40f94b401c87b1
Ramses di Perna <15826@ma-web.nl>
2014-06-04 22:43:20 +0200

    Made portal and portal script. also made the spawnsystem so that the portal is part of it nad made the gameplay 1000 better

--- Assets/Scripts/Background/BackgroundSwithcing.cs
+++ Assets/Scripts/Background/BackgroundSwithcing.cs
@@ -12,7 +12,7 @@ public class BackgroundSwithcing : MonoBehaviour {
 	void Awake () {
 		nrBackgrounds = backgrounds.Length;
 		currentBackground = nrBackgrounds + 1; //anders kiest hij de eerste niet
-		changeLevel (); // <------------ voor test van background change. Moet vervangen worden met "chooseBackground"
+		chooseBackground (); // <------------ voor test van background change. Moet vervangen worden met "chooseBackground"
 	}
 
 	public void chooseBackground(){
--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -8,6 +8,10 @@ public class EnemySpawner : MonoBehaviour {
 	public GameObject enemy1Num2;
 	public GameObject enemy2Num2;
 
+	public GameObject portal;
+
+	int portalCounter;
+
 	void Start() {
 
 		Invoke("EnemySpawn",5f);
@@ -29,7 +33,7 @@ public class EnemySpawner : MonoBehaviour {
 
 	void EnemySpawn() {
 
-		int chooseSpawnType = Random.Range (1, 4);
+		int chooseSpawnType = Random.Range (1, 5);
 		int chooseEnemyToSpawn = Random.Range(1, 3);
 
 		Debug.Log(chooseEnemyToSpawn);
@@ -45,15 +49,20 @@ public class EnemySpawner : MonoBehaviour {
 			} else if (chooseEnemyToSpawn == 2) {
 				Spawn2Enemies (enemy2,enemy1);
 			}	
-		}else{
+		}else if (chooseSpawnType == 3){
 			if(chooseEnemyToSpawn == 1){
 				SpawnEnemy(enemy1Num2);
 			}else if(chooseEnemyToSpawn == 2){
 				SpawnEnemy(enemy2Num2);
 			}
+		}else{
+			portalCounter ++;
+			if(portalCounter == 5){
+				portalCounter = 0;
+				Instantiate(portal, new Vector2 (4, 0.48f), this.transform.rotation);
+			}
 		}
 
-
-		Invoke("EnemySpawn",Random.Range(5f,7f));
+		Invoke("EnemySpawn",Random.Range(5f - portalCounter,7f - portalCounter));
 	}
 }
